924b017b0a34478b30a75954ca8ec4adaa10a2e1
Sanchit Rastogi <rastogi1sanch@gmail.com>
2026-06-27 20:09:30 +0530

    added console

--- src/app/Global/Header/Header.js
+++ src/app/Global/Header/Header.js
@@ -1,35 +1,36 @@
 import Navbaar from "./Component/Navbaar";
 
 const fetchData = async () => {
+  console.log("jwt-----------", process.env.JWT_SECRET);
   try {
-    const [mainMenuRes, subMenuRes, linkMenuRes,SITE_DATA] = await Promise.all([
+    const [mainMenuRes, subMenuRes, linkMenuRes, SITE_DATA] = await Promise.all([
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/MainMenu`, {
         headers: {
           'Content-Type': 'application/json',
           'authorization': `Bearer ${process.env.JWT_SECRET}`,
         },
-          cache: 'no-store'
+        cache: 'no-store'
       }),
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/SubMenu`, {
         headers: {
           'Content-Type': 'application/json',
           'authorization': `Bearer ${process.env.JWT_SECRET}`,
         },
-          cache: 'no-store'
+        cache: 'no-store'
       }),
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/SubMenuLinks`, {
         headers: {
           'Content-Type': 'application/json',
           'authorization': `Bearer ${process.env.JWT_SECRET}`,
         },
-          cache: 'no-store'
+        cache: 'no-store'
       }),
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/SITE_DATA/1`, {
         headers: {
           'Content-Type': 'application/json',
           'authorization': `Bearer ${process.env.JWT_SECRET}`,
         },
-          cache: 'no-store'
+        cache: 'no-store'
       }),
     ]);
 
@@ -37,24 +38,24 @@ const fetchData = async () => {
       throw new Error("Failed to fetch menu data");
     }
 
-    const [mainMenu, subMenu, linkMenu,SITE_DATA1] = await Promise.all([
+    const [mainMenu, subMenu, linkMenu, SITE_DATA1] = await Promise.all([
       mainMenuRes.json(),
       subMenuRes.json(),
       linkMenuRes.json(),
       SITE_DATA.json()
     ]);
 
-    return { mainMenu, subMenu, linkMenu,SITE_DATA1 };
+    return { mainMenu, subMenu, linkMenu, SITE_DATA1 };
   } catch (error) {
     console.error("Error fetching data:", error);
-    return { mainMenu: [], subMenu: [], linkMenu: [],SITE_DATA1:[] }; // Return empty arrays in case of error
+    return { mainMenu: [], subMenu: [], linkMenu: [], SITE_DATA1: [] }; // Return empty arrays in case of error
   }
 };
 
 const Header = async () => {
   const data = await fetchData();
-  
-   // Debugging purpose
+
+  // Debugging purpose
 
   return <Navbaar data={data} SITE_DATA={data.SITE_DATA1} />;
 };
